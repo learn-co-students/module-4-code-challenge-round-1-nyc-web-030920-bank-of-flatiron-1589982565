e96ab5236ee5701b2f504a54e0a498f9f9810cfa
Ramzy Badawy <Ramzy.Badawy@gmail.com>
2020-05-20 10:19:13 -0400

    fetch

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,32 @@ import AccountContainer from "./AccountContainer";
 import "../stylesheets/App.css";
 
 class App extends Component {
+
+  state = {
+    transactions: this.state
+  }
+  componentDidMount(){
+    fetch("http://localhost:6001/transactions")
+    .then(response => response.json())
+    // .then(data => console.log(data))
+    .then(data => this.setState({transactions: data}))
+
+  }
   render() {
+    // console.log(this.state.transactions)
     return (
       <div className="ui raised segment">
         <div className="ui segment violet inverted">
           <h2>The Royal Bank of Flatiron</h2>
         </div>
-        <AccountContainer />
+        <AccountContainer transactions={this.state.transactions}/>
       </div>
     );
   }
 }
 
 export default App;
+
+
+// App
+//  /   \
