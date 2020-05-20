b27f428e9f0bc30d44247861e33d48d93d9362ad
Ramzy Badawy <Ramzy.Badawy@gmail.com>
2020-05-20 11:29:49 -0400

    see you in round 2

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,28 +1,30 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
+  {this.props.transactions.map((transaction) => <Transaction key={transaction.id} {...transaction}/>)}
+
   return (
+    
     <table className="ui celled striped padded table">
       <tbody>
         <tr>
           <th>
-            <h3 className="ui center aligned header">Date</h3>
+            <h3 className="ui center aligned header">Date </h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">Description</h3>
+           <h3 className="ui center aligned header">Description</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">Category</h3>
+           <h3 className="ui center aligned header">Category</h3>
           </th>
           <th>
             <h3 className="ui center aligned header">Amount</h3>
           </th>
-        </tr>
-        {/* {this.props.transactions.map(transaction) => <Transaction key={transaction.id} {...transaction}}} */}
+        </tr >
       </tbody>
     </table>
   );
-};
+  };
 
 export default TransactionsList;
