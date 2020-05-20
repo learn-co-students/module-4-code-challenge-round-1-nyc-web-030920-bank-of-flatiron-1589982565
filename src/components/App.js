7a1fd2dfe4dae34a41fb229ad6cf38c17bcacdd5
Leon Jamison <leonjamison@icloud.com>
2020-05-20 10:25:01 -0400

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,33 @@ import React, { Component } from "react";
 import AccountContainer from "./AccountContainer";
 import "../stylesheets/App.css";
 
+const api_backend = `http://localhost:6001/transactions`
+
 class App extends Component {
+
+  state= {
+    transactions: []
+  }
+
+  componentDidMount(){
+    fetch(api_backend)
+    .then(r=>r.json())
+    .then(transactions => this.setState({transactions}))
+  }
+
+
+
+
+
   render() {
+
+    console.log('app state:', this.state)
     return (
       <div className="ui raised segment">
         <div className="ui segment violet inverted">
           <h2>The Royal Bank of Flatiron</h2>
         </div>
-        <AccountContainer />
+        <AccountContainer transactions={this.state.transactions} />
       </div>
     );
   }
