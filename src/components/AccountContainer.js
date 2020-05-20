6f2d2ff16187598ed12004c64157678db9a867fc
Yueying <wangyy395@gmail.com>
2020-05-20 10:12:40 -0400

    render transaction list

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -2,14 +2,33 @@ import React, { Component } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+const transactionsUrl = 'http://localhost:6001/transactions'
 
 class AccountContainer extends Component {
+
+  state = {
+    transactions: [],
+    search: '',
+    newTransaction: {
+      date: null,
+      description: '',
+      category: '',
+      amount: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch(transactionsUrl)
+      .then(res => res.json())
+      .then(transactions => this.setState({transactions}))
+  }
   render() {
+    console.log("Trans:", this.state)
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList transactions={this.state.transactions}/>
       </div>
     );
   }
