df0daae9a2c3d60aad20f6f1225f88c9023e8dcd
duruchima <duruchima1@gmail.com>
2020-05-20 11:18:38 -0400

    working on delete

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,28 @@
-import React from "react";
+// import React from "react";
 
-const Transaction = (props) => {
-  return (
-    <tr>
-      <td>{props.transaction.date}</td>
-      <td>{props.transaction.description}</td>
-      <td>{props.transaction.category}</td>
-      <td>{props.transaction.amount}</td>
-    </tr>
-  );
-};
+// const Transaction = (props) => {
+//   return (
+//     <tr>
+//       <td>{props.transaction.date}</td>
+//       <td>{props.transaction.description}</td>
+//       <td>{props.transaction.category}</td>
+//       <td>{props.transaction.amount}</td>
+//     </tr>
+//   );
+// };
 
-export default Transaction;
+// export default Transaction;
+import React, { Component } from 'react';
+export default class Transaction extends Component {
+  state = {
+    clicked: false
+  }
+  toggleClicked = () => this.setState({clicked: true})
+  render(){
+    const 
+  }
+
+
+
+
+}
